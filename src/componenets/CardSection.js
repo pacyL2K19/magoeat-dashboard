8fc9a9cfd4eb1bea7e317b4d8aaf917bb357d39b
Pacifique Linjanja <pacilinja1@icloud.com>
2020-08-14 23:34:03 +0200

    Minor update for testing helper file

--- src/componenets/CardSection.js
+++ src/componenets/CardSection.js
@@ -7,7 +7,7 @@ import {
     Card
 } from '@uifabric/react-cards';
 import 'office-ui-fabric-react/dist/css/fabric.css';
-import cards from './helpers/cards'
+// import cards from './helpers/cards'
 
 const container = {
     display : 'flex',
@@ -55,6 +55,33 @@ const styles = {
         }
     }
 };
+
+const cards = [
+    {
+        title : 'TOTAL AMOUNT',
+        amount : '$10,000',
+        icon : 'Money',
+        percentage : '3.3',
+    },
+    {
+        title : 'TOTAL USERS',
+        amount : '10,000,000',
+        icon : 'People',
+        percentage : '2.3',
+    },
+    {
+        title : 'TOTAL RESTAURANTS',
+        amount : '13',
+        icon : 'EatDrink',
+        percentage : '4.5',
+    },
+    {
+        title : 'PROFITS',
+        amount : '$10,0004',
+        icon : 'BankSolid',
+        percentage : '7.1',
+    },
+];
 
 const CardsSection = () => {
     initializeIcons();
