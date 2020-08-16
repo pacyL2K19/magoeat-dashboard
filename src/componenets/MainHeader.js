566eb5ef0447af867a1e9a5e0cb1b3375832a482
Pacifique Linjanja <pacilinja1@icloud.com>
2020-08-16 03:23:01 +0200

    Implementing mainHeader component

--- src/componenets/MainHeader.js
+++ src/componenets/MainHeader.js
@@ -1,1 +1,16 @@
-import React from 'react'
+import React from 'react';
+import 'primeicons/primeicons.css'
+import '../styles/mainHeader.css'
+
+const MainHeader = () => {
+    return (
+        <div className = 'root'>
+            <img alt = 'MagoEat' src = '../assets/logo2.png' />
+            <div className = 'controlZone'>
+                <i className = 'pi pi-sign-out' style = {{ fontSize : '2em' }}></i>
+            </div>
+        </div>
+    )
+}
+
+export default MainHeader;
