bdd683f5653ac5da6bff0d943f86ccf5bfe2c84f
Pacifique Linjanja <pacilinja1@icloud.com>
2020-08-19 20:27:56 +0200

    Minor update

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import React from 'react';
 import 'office-ui-fabric-react/dist/css/fabric.css';
 import './App.css';
 
-import Navigation from '../components/Navigation';
-import CardSection from '../components/CardSection';
-import MainHeader from '../components/MainHeader';
+import Navigation from './components/Navigation';
+import CardSection from './components/CardSection';
+import MainHeader from './components/MainHeader';
 
 function App() {
     return (
--- src/components/MainHeader.js
+++ src/components/MainHeader.js
@@ -1,13 +1,29 @@
 import React from 'react';
 import 'primeicons/primeicons.css'
 import '../styles/mainHeader.css'
+import logo from '../assets/logo2.png'
 
+const styles = {
+    root : {
+        backgroundColor : '#fff116',
+        flex:  1,
+        flexDirection : 'row',
+        padding : 10
+    },
+    logo : {
+        flex : 1,
+    }
+}
 const MainHeader = () => {
+    
     return (
-        <div className = 'root'>
-            <img alt = 'MagoEat' src = '../assets/logo2.png' />
+        <div className = 'root' style = {styles.root}>
+            <div style = {styles.logo}>
+                <img alt = 'MagoEat' src = {logo} />
+            </div>
+            
             <div className = 'controlZone'>
-                <i className = 'pi pi-sign-out' style = {{ fontSize : '2em' }}></i>
+                <i className = 'pi pi-sign-out' style = {{ fontSize : '1.5em' }}></i>
             </div>
         </div>
     )
