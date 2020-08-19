a5f42954ecde2c522a6aca40d3f7dafa1867bfd5
Pacifique Linjanja <pacilinja1@icloud.com>
2020-08-19 20:04:27 +0200

    First test

--- src/App.js
+++ src/App.js
@@ -3,22 +3,13 @@ import 'office-ui-fabric-react/dist/css/fabric.css';
 import './App.css';
 
 import Navigation from './componenets/Navigation';
-import CardSection from './componenets/CardSection'
+import CardSection from './componenets/CardSection';
+import MainHeader from './components/MainHeader';
 
 function App() {
     return (
         <div className = 'ms-Grid' dir = 'ltr'>  
-          <div className='ms-Grid-row'>
-            <div className = 'ms-Grid-coln ms-sm1 ms-xl1'>
-              <Navigation />
-            </div>
-            <div className = 'main-element ms-Grid-coln ms-sm11 ms-xl11'>
-              <CardSection />
-            </div>
-            <div className = 'ms-Grid-row'>
-
-            </div>
-          </div>
+          <MainHeader />
         </div>
     );
 }
