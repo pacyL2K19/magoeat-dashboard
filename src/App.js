1928ec14dbef700c42bb9676ccac672831867e9d
Pacifique Linjanja <pacilinja1@icloud.com>
2020-08-21 00:54:42 +0200

    Preparing app to receive components

--- src/App.js
+++ src/App.js
@@ -7,9 +7,41 @@ import CardSection from './components/CardSection';
 import MainHeader from './components/MainHeader';
 
 function App() {
+  const styles = {
+    root : {
+
+    },
+    body : {
+      display : 'flex',
+      flexDirection : 'row',
+    },
+    nav : {
+      width : '300px',
+    },
+    main : {
+      backgroundColor : '#efefef',
+      display : 'flex'
+    }
+  }
     return (
         <div className = 'ms-Grid' dir = 'ltr'>  
           <MainHeader />
+          <div style = {styles.body}>
+            <div style = {styles.nav}>
+              {
+                /**
+                 * The navigation component 
+                 */
+              }
+            </div>
+            <div style = {styles.main}>
+              {
+                /**
+                 * The main component, depending on the selected item refering to the nav component
+                 */
+              }
+            </div>
+          </div>
         </div>
     );
 }
