e3a4cb83dc5a51f6c1168db486fad36a753cbfa2
Pacifique Linjanja <pacilinja1@icloud.com>
2020-08-21 21:18:54 +0200

    Preparing connectedUser component

--- src/components/ConnectedUser.js
+++ src/components/ConnectedUser.js
@@ -1,25 +1,48 @@
 import React from 'react';
 
-const ConnectedUser = () => {
-    return (
-        <div className = 'root' style = {styles.root}> 
-            <img alt = 'Admin' source = '../assets/Me.png' style = {styles.avatar}/>
-            <div>
-                <h2>Pacifique LINJANJA</h2>
-                <h3>pacilinja2@gmail.com</h3>
-                <h4>CTO Manager</h4>
+class ConnectedUser extends React.Component {
+    constructor (props) {
+        super(props);
+        this.state = {
+            connectedUser : []
+        }
+    }
+    componentDidMount () {
+        // to get the connected user and open up a session
+
+    }
+    render () {
+        return (
+            <div className = 'root' style = {styles.root}> 
+                <img alt = 'Admin' source = '../assets/Me.png' style = {styles.avatar}/>
+                <div>
+                    <h2>{this.state.connectedUser.username}</h2>
+                    <h3>{this.state.connectedUser.mail}</h3>
+                    <h4>{this.state.connectedUser.role}</h4>
+                </div>
+                <div style = {styles.separator}></div>
             </div>
-        </div>
-    )
+        )
+    }
 }
 
 const styles = {
     root : {
         flex: 1,
-        padding: 15,
+        padding: '15px',
     },
     avatar : {
-        
+        height : '50px',
+        width : '50px',
+        borderRaduis : '25px',
+        borderColor : '##00ef',
+        borderWidth : '2px'
+    },
+    separator : {
+        height : '3px',
+        width : '80%',
+        alignSelf : 'center',
+        backgroundColor : '#efefaa'
     }
 }
 
