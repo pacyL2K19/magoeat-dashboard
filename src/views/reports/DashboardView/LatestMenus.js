faf91647687b780bdd0c5067207e465cba29dfd1
Pacifique Linjanja <pacilinja1@icloud.com>
2020-09-09 16:28:29 +0200

    Adding some helper files before api consumption

---
+++ src/views/reports/DashboardView/LatestMenus.js
@@ -0,0 +1,128 @@
+import React, { useState } from 'react';
+import clsx from 'clsx';
+import PropTypes from 'prop-types';
+import { v4 as uuid } from 'uuid';
+import moment from 'moment';
+import {
+  Box,
+  Button,
+  Card,
+  CardHeader,
+  Divider,
+  IconButton,
+  List,
+  ListItem,
+  ListItemAvatar,
+  ListItemText,
+  makeStyles
+} from '@material-ui/core';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import ArrowRightIcon from '@material-ui/icons/ArrowRight';
+
+const data = [
+  {
+    id: uuid(),
+    name: 'Crepe | Lappa',
+    imageUrl: '/static/images/plats/crepe.jpeg',
+    updatedAt: moment().subtract(2, 'hours')
+  },
+  {
+    id: uuid(),
+    name: 'Frites | La Liga',
+    imageUrl: '/static/images/plats/frites.jpeg',
+    updatedAt: moment().subtract(2, 'hours')
+  },
+  {
+    id: uuid(),
+    name: 'Hamburger | Les volcans',
+    imageUrl: '/static/images/plats/hamburger.png',
+    updatedAt: moment().subtract(3, 'hours')
+  },
+  {
+    id: uuid(),
+    name: 'Pizza | Lappa',
+    imageUrl: '/static/images/plats/pizza.jpg',
+    updatedAt: moment().subtract(5, 'hours')
+  },
+  {
+    id: uuid(),
+    name: 'Shawarma | Maghali',
+    imageUrl: '/static/images/plats/shawarma.jpg',
+    updatedAt: moment().subtract(9, 'hours')
+  }
+];
+
+const useStyles = makeStyles(({
+  root: {
+    height: '100%'
+  },
+  image: {
+    height: 48,
+    width: 48
+  }
+}));
+
+const LatestMenus = ({ className, ...rest }) => {
+  const classes = useStyles();
+  const [products] = useState(data);
+
+  return (
+    <Card
+      className={clsx(classes.root, className)}
+      {...rest}
+    >
+      <CardHeader
+        subtitle={`${products.length} in total`}
+        title="Latest Plats"
+      />
+      <Divider />
+      <List>
+        {products.map((product, i) => (
+          <ListItem
+            divider={i < products.length - 1}
+            key={product.id}
+          >
+            <ListItemAvatar>
+              <img
+                alt="Product"
+                className={classes.image}
+                src={product.imageUrl}
+              />
+            </ListItemAvatar>
+            <ListItemText
+              primary={product.name}
+              secondary={`Updated ${product.updatedAt.fromNow()}`}
+            />
+            <IconButton
+              edge="end"
+              size="small"
+            >
+              <MoreVertIcon />
+            </IconButton>
+          </ListItem>
+        ))}
+      </List>
+      <Divider />
+      <Box
+        display="flex"
+        justifyContent="flex-end"
+        p={2}
+      >
+        <Button
+          color="primary"
+          endIcon={<ArrowRightIcon />}
+          size="small"
+          variant="text"
+        >
+          View all
+        </Button>
+      </Box>
+    </Card>
+  );
+};
+
+LatestMenus.propTypes = {
+  className: PropTypes.string
+};
+
+export default LatestMenus;
---
+++ src/views/reports/DashboardView/LatestRestaurants.js
@@ -0,0 +1,128 @@
+import React, { useState } from 'react';
+import clsx from 'clsx';
+import PropTypes from 'prop-types';
+import { v4 as uuid } from 'uuid';
+import moment from 'moment';
+import {
+  Box,
+  Button,
+  Card,
+  CardHeader,
+  Divider,
+  IconButton,
+  List,
+  ListItem,
+  ListItemAvatar,
+  ListItemText,
+  makeStyles
+} from '@material-ui/core';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import ArrowRightIcon from '@material-ui/icons/ArrowRight';
+
+const data = [
+  {
+    id: uuid(),
+    name: 'Havana',
+    imageUrl: '/static/images/products/havana.jpeg',
+    updatedAt: moment().subtract(2, 'hours')
+  },
+  {
+    id: uuid(),
+    name: 'La Liga',
+    imageUrl: '/static/images/products/liga.jpg',
+    updatedAt: moment().subtract(2, 'hours')
+  },
+  {
+    id: uuid(),
+    name: 'Les volcans',
+    imageUrl: '/static/images/products/volcan.jpeg',
+    updatedAt: moment().subtract(3, 'hours')
+  },
+  {
+    id: uuid(),
+    name: 'Lappa',
+    imageUrl: '/static/images/products/lappa.jpeg',
+    updatedAt: moment().subtract(5, 'hours')
+  },
+  {
+    id: uuid(),
+    name: 'Maghali',
+    imageUrl: '/static/images/products/product_5.png',
+    updatedAt: moment().subtract(9, 'hours')
+  }
+];
+
+const useStyles = makeStyles(({
+  root: {
+    height: '100%'
+  },
+  image: {
+    height: 48,
+    width: 48
+  }
+}));
+
+const LatestRestaurants = ({ className, ...rest }) => {
+  const classes = useStyles();
+  const [products] = useState(data);
+
+  return (
+    <Card
+      className={clsx(classes.root, className)}
+      {...rest}
+    >
+      <CardHeader
+        subtitle={`${products.length} in total`}
+        title="Latest Restaurants"
+      />
+      <Divider />
+      <List>
+        {products.map((product, i) => (
+          <ListItem
+            divider={i < products.length - 1}
+            key={product.id}
+          >
+            <ListItemAvatar>
+              <img
+                alt="Product"
+                className={classes.image}
+                src={product.imageUrl}
+              />
+            </ListItemAvatar>
+            <ListItemText
+              primary={product.name}
+              secondary={`Updated ${product.updatedAt.fromNow()}`}
+            />
+            <IconButton
+              edge="end"
+              size="small"
+            >
+              <MoreVertIcon />
+            </IconButton>
+          </ListItem>
+        ))}
+      </List>
+      <Divider />
+      <Box
+        display="flex"
+        justifyContent="flex-end"
+        p={2}
+      >
+        <Button
+          color="primary"
+          endIcon={<ArrowRightIcon />}
+          size="small"
+          variant="text"
+        >
+          View all
+        </Button>
+      </Box>
+    </Card>
+  );
+};
+
+LatestRestaurants.propTypes = {
+  className: PropTypes.string
+};
+
+export default LatestRestaurants;
