87c3313ecd16704b0c1dbff379cacccecdcf23f5
Pacifique LINJANJA <pacilinja1@icloud.com>
2021-03-29 22:28:01 +0200

    Update the account files

--- src/views/account/AccountView/index.js
+++ src/views/account/AccountView/index.js
@@ -1,55 +1,56 @@
-import React from 'react';
+/* eslint-disable no-unused-vars */
+import React from "react";
 import {
-  Container,
-  Grid,
-  makeStyles
-} from '@material-ui/core';
-import Page from '../../../components/Page';
-import Profile from './Profile';
-import ProfileDetails from './ProfileDetails';
+    Container,
+    Grid,
+    makeStyles
+} from "@material-ui/core";
+import Page from "../../../components/Page";
+import Profile from "./Profile";
+import ProfileDetails from "./ProfileDetails";
 
 const useStyles = makeStyles((theme) => ({
-  root: {
-    backgroundColor: theme.palette.background.dark,
-    minHeight: '100%',
-    paddingBottom: theme.spacing(3),
-    paddingTop: theme.spacing(3)
-  }
+    root: {
+        backgroundColor: theme.palette.background.dark,
+        minHeight: "100%",
+        paddingBottom: theme.spacing(3),
+        paddingTop: theme.spacing(3)
+    }
 }));
 
 const Account = () => {
-  const classes = useStyles();
+    const classes = useStyles();
 
-  return (
-    <Page
-      className={classes.root}
-      title="Account"
-    >
-      <Container maxWidth="lg">
-        <Grid
-          container
-          spacing={3}
+    return (
+        <Page
+            className={classes.root}
+            title="Account"
         >
-          <Grid
-            item
-            lg={4}
-            md={6}
-            xs={12}
-          >
-            <Profile />
-          </Grid>
-          <Grid
-            item
-            lg={8}
-            md={6}
-            xs={12}
-          >
-            <ProfileDetails />
-          </Grid>
-        </Grid>
-      </Container>
-    </Page>
-  );
+            <Container maxWidth="lg">
+                <Grid
+                    container
+                    spacing={3}
+                >
+                    <Grid
+                        item
+                        lg={4}
+                        md={6}
+                        xs={12}
+                    >
+                        <Profile />
+                    </Grid>
+                    <Grid
+                        item
+                        lg={8}
+                        md={6}
+                        xs={12}
+                    >
+                        <ProfileDetails />
+                    </Grid>
+                </Grid>
+            </Container>
+        </Page>
+    );
 };
 
 export default Account;
