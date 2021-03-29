717558aab429fbadeeae991adcd524f41c3b60f5
Pacifique LINJANJA <pacilinja1@icloud.com>
2021-03-29 22:20:39 +0200

    Fix eslint warning in the navbar files

--- src/layouts/DashboardLayout/NavBar/index.js
+++ src/layouts/DashboardLayout/NavBar/index.js
@@ -1,188 +1,188 @@
-import React, { useEffect } from 'react';
-import { Link as RouterLink, useLocation } from 'react-router-dom';
-import PropTypes from 'prop-types';
+/* eslint-disable no-unused-vars */
+import React, { useEffect } from "react";
+import { Link as RouterLink, useLocation } from "react-router-dom";
+import PropTypes from "prop-types";
 import {
-  Avatar,
-  Box,
-  // Button,
-  Divider,
-  Drawer,
-  Hidden,
-  List,
-  Typography,
-  makeStyles
-} from '@material-ui/core';
+    Avatar,
+    Box,
+    Button,
+    Divider,
+    Drawer,
+    Hidden,
+    List,
+    Typography,
+    makeStyles
+} from "@material-ui/core";
 import {
-  // AlertCircle as AlertCircleIcon,
-  BarChart as BarChartIcon,
-  Lock as LockIcon,
-  Settings as SettingsIcon,
-  ShoppingBag as ShoppingBagIcon,
-  User as UserIcon,
-  UserPlus as UserPlusIcon,
-  Users as UsersIcon
-} from 'react-feather';
-import NavItem from './NavItem';
+    // AlertCircle as AlertCircleIcon,
+    BarChart as BarChartIcon,
+    Lock as LockIcon,
+    Settings as SettingsIcon,
+    ShoppingBag as ShoppingBagIcon,
+    User as UserIcon,
+    UserPlus as UserPlusIcon,
+    Users as UsersIcon
+} from "react-feather";
+import NavItem from "./NavItem";
 
 const user = {
-  avatar: '/static/images/avatars/Me.jpeg.png', // to retrieve from the backend
-  jobTitle: 'CTO | FullStack Software Eng',
-  name: 'Pacifique Linjanja'
-}; // to retrieve from the backend
+    avatar: "/static/images/avatars/Me.jpeg.png", // to retrieve from the backend
+    jobTitle: "CTO | FullStack Software Eng",
+    name: "Pacifique Linjanja"
+};
 
 const items = [
-  {
-    href: '/app/dashboard',
-    icon: BarChartIcon,
-    title: 'Dashboard'
-  },
-  {
-    href: '/app/customers',
-    icon: UsersIcon,
-    title: 'Customers'
-  },
-  {
-    href: '/app/products',
-    icon: ShoppingBagIcon,
-    title: 'Restaurants'
-  },
-  {
-    href: '/app/account',
-    icon: UserIcon,
-    title: 'Account'
-  },
-  {
-    href: '/app/settings',
-    icon: SettingsIcon,
-    title: 'Settings'
-  },
-  {
-    href: '/login',
-    icon: LockIcon,
-    title: 'Login'
-  },
-  {
-    href: '/register',
-    icon: UserPlusIcon,
-    title: 'Register'
-  },
-  // {
-  //   href: '/404',
-  //   icon: AlertCircleIcon,
-  //   title: 'Error'
-  // }
+    {
+        href: "/app/dashboard",
+        icon: BarChartIcon,
+        title: "Dashboard"
+    },
+    {
+        href: "/app/customers",
+        icon: UsersIcon,
+        title: "Customers"
+    },
+    {
+        href: "/app/products",
+        icon: ShoppingBagIcon,
+        title: "Restaurants"
+    },
+    {
+        href: "/app/account",
+        icon: UserIcon,
+        title: "Account"
+    },
+    {
+        href: "/app/settings",
+        icon: SettingsIcon,
+        title: "Settings"
+    },
+    {
+        href: "/login",
+        icon: LockIcon,
+        title: "Login"
+    },
+    {
+        href: "/register",
+        icon: UserPlusIcon,
+        title: "Register"
+    },
+    // {
+    //   href: '/404',
+    //   icon: AlertCircleIcon,
+    //   title: 'Error'
+    // }
 ];
 
 const useStyles = makeStyles(() => ({
-  mobileDrawer: {
-    width: 256
-  },
-  desktopDrawer: {
-    width: 256,
-    top: 64,
-    height: 'calc(100% - 64px)'
-  },
-  avatar: {
-    cursor: 'pointer',
-    width: 64,
-    height: 64
-  }
+    mobileDrawer: {
+        width: 256
+    },
+    desktopDrawer: {
+        width: 256,
+        top: 64,
+        height: "calc(100% - 64px)"
+    },
+    avatar: {
+        cursor: "pointer",
+        width: 64,
+        height: 64
+    }
 }));
 
 const NavBar = ({ onMobileClose, openMobile }) => {
-  const classes = useStyles();
-  const location = useLocation();
+    const classes = useStyles();
+    const location = useLocation();
 
-  useEffect(() => {
-    if (openMobile && onMobileClose) {
-      onMobileClose();
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [location.pathname]);
+    useEffect(() => {
+        if (openMobile && onMobileClose) {
+            onMobileClose();
+        }
+    }, [location.pathname]);
 
-  const content = (
-    <Box
-      height="100%"
-      display="flex"
-      flexDirection="column"
-    >
-      <Box
-        alignItems="center"
-        display="flex"
-        flexDirection="column"
-        p={2}
-      >
-        <Avatar
-          className={classes.avatar}
-          component={RouterLink}
-          src={user.avatar}
-          to="/app/account"
-        />
-        <Typography
-          className={classes.name}
-          color="textPrimary"
-          variant="h5"
+    const content = (
+        <Box
+            height="100%"
+            display="flex"
+            flexDirection="column"
         >
-          {user.name}
-        </Typography>
-        <Typography
-          color="textSecondary"
-          variant="body2"
-        >
-          {user.jobTitle}
-        </Typography>
-      </Box>
-      <Divider />
-      <Box p={2}>
-        <List>
-          {items.map((item) => (
-            <NavItem
-              href={item.href}
-              key={item.title}
-              title={item.title}
-              icon={item.icon}
-            />
-          ))}
-        </List>
-      </Box>
-    </Box>
-  );
+            <Box
+                alignItems="center"
+                display="flex"
+                flexDirection="column"
+                p={2}
+            >
+                <Avatar
+                    className={classes.avatar}
+                    component={RouterLink}
+                    src={user.avatar}
+                    to="/app/account"
+                />
+                <Typography
+                    className={classes.name}
+                    color="textPrimary"
+                    variant="h5"
+                >
+                    {user.name}
+                </Typography>
+                <Typography
+                    color="textSecondary"
+                    variant="body2"
+                >
+                    {user.jobTitle}
+                </Typography>
+            </Box>
+            <Divider />
+            <Box p={2}>
+                <List>
+                    {items.map((item) => (
+                        <NavItem
+                            href={item.href}
+                            key={item.title}
+                            title={item.title}
+                            icon={item.icon}
+                        />
+                    ))}
+                </List>
+            </Box>
+        </Box>
+    );
 
-  return (
-    <>
-      <Hidden lgUp>
-        <Drawer
-          anchor="left"
-          classes={{ paper: classes.mobileDrawer }}
-          onClose={onMobileClose}
-          open={openMobile}
-          variant="temporary"
-        >
-          {content}
-        </Drawer>
-      </Hidden>
-      <Hidden mdDown>
-        <Drawer
-          anchor="left"
-          classes={{ paper: classes.desktopDrawer }}
-          open
-          variant="persistent"
-        >
-          {content}
-        </Drawer>
-      </Hidden>
-    </>
-  );
+    return (
+        <>
+            <Hidden lgUp>
+                <Drawer
+                    anchor="left"
+                    classes={{ paper: classes.mobileDrawer }}
+                    onClose={onMobileClose}
+                    open={openMobile}
+                    variant="temporary"
+                >
+                    {content}
+                </Drawer>
+            </Hidden>
+            <Hidden mdDown>
+                <Drawer
+                    anchor="left"
+                    classes={{ paper: classes.desktopDrawer }}
+                    open
+                    variant="persistent"
+                >
+                    {content}
+                </Drawer>
+            </Hidden>
+        </>
+    );
 };
 
 NavBar.propTypes = {
-  onMobileClose: PropTypes.func,
-  openMobile: PropTypes.bool
+    onMobileClose: PropTypes.func,
+    openMobile: PropTypes.bool
 };
 
 NavBar.defaultProps = {
-  onMobileClose: () => {},
-  openMobile: false
+    onMobileClose: () => {},
+    openMobile: false
 };
 
 export default NavBar;
