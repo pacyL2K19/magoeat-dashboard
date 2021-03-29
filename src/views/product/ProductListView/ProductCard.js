6361f70972e78c9490da54c207e99ecfbba6c535
Pacifique LINJANJA <pacilinja1@icloud.com>
2021-03-29 22:32:17 +0200

    Fix linter warning inthe product module

--- src/views/product/ProductListView/ProductCard.js
+++ src/views/product/ProductListView/ProductCard.js
@@ -1,127 +1,128 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import clsx from 'clsx';
+/* eslint-disable no-unused-vars */
+import React from "react";
+import PropTypes from "prop-types";
+import clsx from "clsx";
 import {
-  Box,
-  Card,
-  CardContent,
-  Divider,
-  Grid,
-  Typography,
-  makeStyles
-} from '@material-ui/core';
-import AccessTimeIcon from '@material-ui/icons/AccessTime';
-import GetAppIcon from '@material-ui/icons/GetApp';
+    Box,
+    Card,
+    CardContent,
+    Divider,
+    Grid,
+    Typography,
+    makeStyles
+} from "@material-ui/core";
+import AccessTimeIcon from "@material-ui/icons/AccessTime";
+import GetAppIcon from "@material-ui/icons/GetApp";
 
 const useStyles = makeStyles((theme) => ({
-  root: {
-    display: 'flex',
-    flexDirection: 'column'
-  },
-  statsItem: {
-    alignItems: 'center',
-    display: 'flex'
-  },
-  statsIcon: {
-    marginRight: theme.spacing(1)
-  }
+    root: {
+        display: "flex",
+        flexDirection: "column"
+    },
+    statsItem: {
+        alignItems: "center",
+        display: "flex"
+    },
+    statsIcon: {
+        marginRight: theme.spacing(1)
+    }
 }));
 
 const ProductCard = ({ className, product, ...rest }) => {
-  const classes = useStyles();
+    const classes = useStyles();
 
-  return (
-    <Card
-      className={clsx(classes.root, className)}
-      {...rest}
-    >
-      <CardContent>
-        <Box
-          display="flex"
-          flexDirection= 'column'
-          justifyContent="center"
-          mb={3}
+    return (
+        <Card
+            className={clsx(classes.root, className)}
+            {...rest}
         >
-          <h2 style={{textAlign: 'center', fontFamily: 'sans-serif'}}>{product.name}</h2>
-          <img
-            src={product.media}
-            style={{
-              height: 100,
-              margin: 10,
-              marginLeft: 'auto',
-              marginRight: 'auto',
-              marginTop: 10,
-              marginBottom: 10
-            }}
-          />
-        </Box>
-        <Typography
-          align="center"
-          color="textPrimary"
-          gutterBottom
-          variant="h4"
-        >
-          {product.title}
-        </Typography>
-        <Typography
-          align="center"
-          color="textPrimary"
-          variant="body1"
-        >
-          {product.description}
-        </Typography>
-      </CardContent>
-      <Box flexGrow={1} />
-      <Divider />
-      <Box p={2}>
-        <Grid
-          container
-          justify="space-between"
-          spacing={2}
-        >
-          <Grid
-            className={classes.statsItem}
-            item
-          >
-            <AccessTimeIcon
-              className={classes.statsIcon}
-              color="action"
-            />
-            <Typography
-              color="textSecondary"
-              display="inline"
-              variant="body2"
-            >
-              {product.createdAt}
-            </Typography>
-          </Grid>
-          <Grid
-            className={classes.statsItem}
-            item
-          >
-            <GetAppIcon
-              className={classes.statsIcon}
-              color="action"
-            />
-            <Typography
-              color="textSecondary"
-              display="inline"
-              variant="body2"
-            >
-              {product.totalOrders}
-              {' '}
+            <CardContent>
+                <Box
+                    display="flex"
+                    flexDirection= 'column'
+                    justifyContent="center"
+                    mb={3}
+                >
+                    <h2 style={{textAlign: "center", fontFamily: "sans-serif"}}>{product.name}</h2>
+                    <img
+                        src={product.media}
+                        style={{
+                            height: 100,
+                            margin: 10,
+                            marginLeft: "auto",
+                            marginRight: "auto",
+                            marginTop: 10,
+                            marginBottom: 10
+                        }}
+                    />
+                </Box>
+                <Typography
+                    align="center"
+                    color="textPrimary"
+                    gutterBottom
+                    variant="h4"
+                >
+                    {product.title}
+                </Typography>
+                <Typography
+                    align="center"
+                    color="textPrimary"
+                    variant="body1"
+                >
+                    {product.description}
+                </Typography>
+            </CardContent>
+            <Box flexGrow={1} />
+            <Divider />
+            <Box p={2}>
+                <Grid
+                    container
+                    justify="space-between"
+                    spacing={2}
+                >
+                    <Grid
+                        className={classes.statsItem}
+                        item
+                    >
+                        <AccessTimeIcon
+                            className={classes.statsIcon}
+                            color="action"
+                        />
+                        <Typography
+                            color="textSecondary"
+                            display="inline"
+                            variant="body2"
+                        >
+                            {product.createdAt}
+                        </Typography>
+                    </Grid>
+                    <Grid
+                        className={classes.statsItem}
+                        item
+                    >
+                        <GetAppIcon
+                            className={classes.statsIcon}
+                            color="action"
+                        />
+                        <Typography
+                            color="textSecondary"
+                            display="inline"
+                            variant="body2"
+                        >
+                            {product.totalOrders}
+                            {" "}
               Orders
-            </Typography>
-          </Grid>
-        </Grid>
-      </Box>
-    </Card>
-  );
+                        </Typography>
+                    </Grid>
+                </Grid>
+            </Box>
+        </Card>
+    );
 };
 
 ProductCard.propTypes = {
-  className: PropTypes.string,
-  product: PropTypes.object.isRequired
+    className: PropTypes.string,
+    product: PropTypes.object.isRequired
 };
 
 export default ProductCard;
--- src/views/product/ProductListView/index.js
+++ src/views/product/ProductListView/index.js
@@ -1,109 +1,110 @@
-import React, { useState, useEffect } from 'react';
-import { v4 as uuid } from 'uuid';
+/* eslint-disable no-unused-vars */
+import React, { useState, useEffect } from "react";
+import { v4 as uuid } from "uuid";
 import {
-  Box,
-  Container,
-  Grid,
-  makeStyles
-} from '@material-ui/core';
-import { Pagination } from '@material-ui/lab';
-import Page from '../../../components/Page';
-import Toolbar from './Toolbar';
-import ProductCard from './ProductCard';
+    Box,
+    Container,
+    Grid,
+    makeStyles
+} from "@material-ui/core";
+import { Pagination } from "@material-ui/lab";
+import Page from "../../../components/Page";
+import Toolbar from "./Toolbar";
+import ProductCard from "./ProductCard";
 // import data from '../../../helpers/data';
 
 const useStyles = makeStyles((theme) => ({
-  root: {
-    backgroundColor: theme.palette.background.dark,
-    minHeight: '100%',
-    paddingBottom: theme.spacing(3),
-    paddingTop: theme.spacing(3)
-  },
-  productCard: {
-    height: '100%'
-  }
+    root: {
+        backgroundColor: theme.palette.background.dark,
+        minHeight: "100%",
+        paddingBottom: theme.spacing(3),
+        paddingTop: theme.spacing(3)
+    },
+    productCard: {
+        height: "100%"
+    }
 }));
 
 const ProductList = () => {
-  const staticUrl='http://localhost:5000/api/restaurants/';
-  const classes = useStyles();
-  const [products, setProducts] = useState([]);
-  useEffect(() => {
-    fetch(staticUrl, {
-      method: 'GET',
-      headers: {
-        'Accept': 'application/json',
-        'Content-type': 'application/json'
-      }
-    })
-      .then(res => res.json())
-      .then(resJson => {
-        let latestRest = [];
-        if (resJson.restaus) {
-          resJson.restaus.map(restau => {
-            let restToAdd = {
-              id: uuid(),
-              name: restau.label,
-              totalOrders: 1,
-              media: restau.imgUrl,
-              description: restau.description,
-              averageRate: restau.averageRating,
-              createdAt: restau.opensAt+" - "+restau.closeAt,
-              // updatedAt: moment().subtract(9, 'hours')
+    const staticUrl="http://localhost:5000/api/restaurants/";
+    const classes = useStyles();
+    const [products, setProducts] = useState([]);
+    useEffect(() => {
+        fetch(staticUrl, {
+            method: "GET",
+            headers: {
+                "Accept": "application/json",
+                "Content-type": "application/json"
             }
-            latestRest.push(restToAdd);
-          })
-          setProducts(latestRest);
-        } else {
-          setProducts([]);
-        }
-      })
-      .catch(() => {
-        setProducts([]);
-      })
-  }, [])
-  return (
-    <Page
-      className={classes.root}
-      title="Products"
-    >
-      <Container maxWidth={false}>
-        <Toolbar />
-        <Box mt={3}>
-          <Grid
-            container
-            spacing={3}
-          >
-            {products.map((product) => (
-              <Grid
-                item
-                key={product.id}
-                lg={4}
-                md={6}
-                xs={12}
-              >
-                <ProductCard
-                  className={classes.productCard}
-                  product={product}
-                />
-              </Grid>
-            ))}
-          </Grid>
-        </Box>
-        <Box
-          mt={3}
-          display="flex"
-          justifyContent="center"
+        })
+            .then(res => res.json())
+            .then(resJson => {
+                let latestRest = [];
+                if (resJson.restaus) {
+                    resJson.restaus.map(restau => {
+                        let restToAdd = {
+                            id: uuid(),
+                            name: restau.label,
+                            totalOrders: 1,
+                            media: restau.imgUrl,
+                            description: restau.description,
+                            averageRate: restau.averageRating,
+                            createdAt: restau.opensAt+" - "+restau.closeAt,
+                            // updatedAt: moment().subtract(9, 'hours')
+                        };
+                        latestRest.push(restToAdd);
+                    });
+                    setProducts(latestRest);
+                } else {
+                    setProducts([]);
+                }
+            })
+            .catch(() => {
+                setProducts([]);
+            });
+    }, []);
+    return (
+        <Page
+            className={classes.root}
+            title="Products"
         >
-          <Pagination
-            color="primary"
-            count={3}
-            size="small"
-          />
-        </Box>
-      </Container>
-    </Page>
-  );
+            <Container maxWidth={false}>
+                <Toolbar />
+                <Box mt={3}>
+                    <Grid
+                        container
+                        spacing={3}
+                    >
+                        {products.map((product) => (
+                            <Grid
+                                item
+                                key={product.id}
+                                lg={4}
+                                md={6}
+                                xs={12}
+                            >
+                                <ProductCard
+                                    className={classes.productCard}
+                                    product={product}
+                                />
+                            </Grid>
+                        ))}
+                    </Grid>
+                </Box>
+                <Box
+                    mt={3}
+                    display="flex"
+                    justifyContent="center"
+                >
+                    <Pagination
+                        color="primary"
+                        count={3}
+                        size="small"
+                    />
+                </Box>
+            </Container>
+        </Page>
+    );
 };
 
 export default ProductList;
