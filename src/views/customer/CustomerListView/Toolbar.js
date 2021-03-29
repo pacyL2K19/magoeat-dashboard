1b49ff46b423b42e6a0fe648f9f916ffdc3bd408
Pacifique LINJANJA <pacilinja1@icloud.com>
2021-03-29 22:29:25 +0200

    Minor update

--- src/views/customer/CustomerListView/Toolbar.js
+++ src/views/customer/CustomerListView/Toolbar.js
@@ -1,84 +1,85 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import clsx from 'clsx';
+/* eslint-disable no-unused-vars */
+import React from "react";
+import PropTypes from "prop-types";
+import clsx from "clsx";
 import {
-  Box,
-  Button,
-  Card,
-  CardContent,
-  TextField,
-  InputAdornment,
-  SvgIcon,
-  makeStyles
-} from '@material-ui/core';
-import { Search as SearchIcon } from 'react-feather';
+    Box,
+    Button,
+    Card,
+    CardContent,
+    TextField,
+    InputAdornment,
+    SvgIcon,
+    makeStyles
+} from "@material-ui/core";
+import { Search as SearchIcon } from "react-feather";
 
 const useStyles = makeStyles((theme) => ({
-  root: {},
-  importButton: {
-    marginRight: theme.spacing(1)
-  },
-  exportButton: {
-    marginRight: theme.spacing(1)
-  }
+    root: {},
+    importButton: {
+        marginRight: theme.spacing(1)
+    },
+    exportButton: {
+        marginRight: theme.spacing(1)
+    }
 }));
 
 const Toolbar = ({ className, ...rest }) => {
-  const classes = useStyles();
+    const classes = useStyles();
 
-  return (
-    <div
-      className={clsx(classes.root, className)}
-      {...rest}
-    >
-      <Box
-        display="flex"
-        justifyContent="flex-end"
-      >
-        <Button className={classes.importButton}>
-          Import
-        </Button>
-        <Button className={classes.exportButton}>
-          Export
-        </Button>
-        <Button
-          color="primary"
-          variant="contained"
+    return (
+        <div
+            className={clsx(classes.root, className)}
+            {...rest}
         >
+            <Box
+                display="flex"
+                justifyContent="flex-end"
+            >
+                <Button className={classes.importButton}>
+          Import
+                </Button>
+                <Button className={classes.exportButton}>
+          Export
+                </Button>
+                <Button
+                    color="primary"
+                    variant="contained"
+                >
           Add customer
-        </Button>
-      </Box>
-      <Box mt={3}>
-        <Card>
-          <CardContent>
-            <Box maxWidth={500}>
-              <TextField
-                fullWidth
-                InputProps={{
-                  startAdornment: (
-                    <InputAdornment position="start">
-                      <SvgIcon
-                        fontSize="small"
-                        color="action"
-                      >
-                        <SearchIcon />
-                      </SvgIcon>
-                    </InputAdornment>
-                  )
-                }}
-                placeholder="Search customer"
-                variant="outlined"
-              />
+                </Button>
             </Box>
-          </CardContent>
-        </Card>
-      </Box>
-    </div>
-  );
+            <Box mt={3}>
+                <Card>
+                    <CardContent>
+                        <Box maxWidth={500}>
+                            <TextField
+                                fullWidth
+                                InputProps={{
+                                    startAdornment: (
+                                        <InputAdornment position="start">
+                                            <SvgIcon
+                                                fontSize="small"
+                                                color="action"
+                                            >
+                                                <SearchIcon />
+                                            </SvgIcon>
+                                        </InputAdornment>
+                                    )
+                                }}
+                                placeholder="Search customer"
+                                variant="outlined"
+                            />
+                        </Box>
+                    </CardContent>
+                </Card>
+            </Box>
+        </div>
+    );
 };
 
 Toolbar.propTypes = {
-  className: PropTypes.string
+    className: PropTypes.string
 };
 
 export default Toolbar;
